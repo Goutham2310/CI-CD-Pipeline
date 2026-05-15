a4c97afc36e883e2786c3888b3291cadb68a6e4c
Gouthanm <goutham.h@hotmail.com>
2026-05-14 21:11:15 -0400

    first commit

---
+++ tests/login.spec.ts
@@ -0,0 +1,15 @@
+import {test, expect, Locator} from '@playwright/test';
+
+test('should login successfully', async ({page}) => {
+  await page.goto('https://www.rbcroyalbank.com/ways-to-bank/online-banking/index.html');
+  await expect(page).toHaveTitle(/RBC Royal Bank/);
+  const clock: Locator = page.getByAltText('Clock icon');
+  await expect(clock).toBeVisible();
+
+  const button: Locator = page.getByRole('button', {name: 'Sign in to R B C Online Banking'});
+  await expect(button).toBeVisible();
+
+  
+
+});
+
